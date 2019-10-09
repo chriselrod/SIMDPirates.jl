08f4907d1fcc7e2707603cd2702608ed399932ad
Chris Elrod <elrodc@gmail.com>
2019-10-09 18:16:27 -0400

    Added a couple tests. Still need to add Test to the dependencies so they can be run, as well as a gazillion more tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
-using SIMDPirates
-using Base.Test
+using SIMDPirates, VectorizationBase
+using Test
 
 
 for T in (Float16, Float32, Float64)
@@ -10,4 +10,12 @@ for T in (Float16, Float32, Float64)
         typemax(uint_type(T))
     @test significand_mask(T) ⊻ exponent_mask(T) ⊻ sign_mask(T) ==
         typemax(uint_type(T))
+
+    W = VectorizationBase.pick_vector_width(T)
+    x = ntuple(W) do _ Core.VecElement(randn(T)) end
+    y = ntuple(W) do _ Core.VecElement(randn(T)) end
+    @test SIMDPirates.vall(
+        SIMDPirates.vgreater_or_equal(x,y) === ntuple(_ -> Core.VecElement(x[w].value >= y[w].value), W)
+    )
+    @test SIMDPirates.vecbool_to_mask( SIMDPirates.vless(x,y) ) === SIMDPirates.vgreater_mask(y, x)
 end
