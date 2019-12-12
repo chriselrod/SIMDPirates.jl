833385049957a788c01b888dd140d7b3bc36a757
Chris Elrod <elrodc@gmail.com>
2019-12-12 08:27:06 -0500

    Updated precompile.

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,7 +1,10 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
+    precompile(Tuple{typeof(SIMDPirates.getneutral),Symbol,Type{Bool}})
+    precompile(Tuple{typeof(SIMDPirates.getneutral),Symbol,Type{Float64}})
     precompile(Tuple{typeof(SIMDPirates.llvmconst),Int64,Type,Float64})
     precompile(Tuple{typeof(SIMDPirates.llvmconst),Int64,Type,Int64})
+    precompile(Tuple{typeof(SIMDPirates.llvmins),Symbol,Int64,Type})
     precompile(Tuple{typeof(SIMDPirates.scalar2vector),String,Int64,String,String})
     precompile(Tuple{typeof(SIMDPirates.subvector),String,Int64,String,String,Int64,Int64})
 end
